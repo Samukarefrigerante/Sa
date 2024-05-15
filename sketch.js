2d7a975eb3696da18808f1ec757b74c439de36c3
Samukarefrigerante <0000111167288xsp@al.educacao.sp.gov.br>
2024-05-15 18:00:04 -0300

    atualizaçao

--- sketch.js
+++ sketch.js
@@ -1,14 +1,36 @@
 function setup() {
+<<<<<<< HEAD
+    createCanvas(400, 400);
+    background("white")
+  }
+=======
   createCanvas(400, 400);
-  background("white")
+  background("black")
 }
 
 function draw() {
   stroke("blue");
   fill("red");
+>>>>>>> d74d25a1544e51acd1d53018567f2c12207c0410
   
+<<<<<<< HEAD
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+=======
   
   if (mouseIsPressed) {
     rect(mouseX, mouseY, 20, 35);
+>>>>>>> d74d25a1544e51acd1d53018567f2c12207c0410
   }
+<<<<<<< HEAD
+  
+=======
 }
+
+>>>>>>> d74d25a1544e51acd1d53018567f2c12207c0410
